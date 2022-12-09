90dbab76914f5bf2fa11d46bc5412c838139801a
Niklas Dettmer <ndettmer94@posteo.de>
2022-12-09 16:59:30 +0100

    re-organized style transfer code, calculating image distribution info

---
+++ CitizenUAV/losses.py
@@ -0,0 +1,39 @@
+from torch import nn
+from torch.nn import functional as F
+
+from CitizenUAV.utils import gram_matrix
+
+
+class StyleLoss(nn.Module):
+    """
+    Source: https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+    """
+
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+        self.loss = None
+
+    def forward(self, x):
+        G = gram_matrix(x)
+        self.loss = F.mse_loss(G, self.target)
+        return x
+
+
+class ContentLoss(nn.Module):
+    """
+    Source: https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+    """
+
+    def __init__(self, target,):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise, the forward method of the criterion
+        # will throw an error.
+        self.target = target.detach()
+        self.loss = None
+
+    def forward(self, x):
+        self.loss = F.mse_loss(x, self.target)
+        return x
--- CitizenUAV/utils.py
+++ CitizenUAV/utils.py
@@ -1,6 +1,10 @@
 import os
+from typing import Tuple
+
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
+import torch
+from tqdm import tqdm
 
 
 def get_pid_from_path(path):
@@ -28,8 +32,45 @@ def read_inat_metadata(data_dir):
     return metadata
 
 
-def ds_mean_std(ds: Dataset):
+def channel_mean_std(ds: Dataset) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Calculates the mean and standard deviation for each channel of an image dataset.
+    :param ds: The image dataset.
+    :return: The means and standard deviations.
+    """
     dl = DataLoader(ds, batch_size=32, num_workers=1)
-    for img, _ in iter(dl):
-        pass
 
+    pixel_counter = 0
+    first_moment = torch.empty(3)
+    second_moment = torch.empty(3)
+
+    for images, _ in tqdm(dl):
+        b, c, h, w = images.shape
+        n_batch_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_squares = torch.sum(images ** 2, dim=[0, 2, 3])
+        first_moment = (pixel_counter * first_moment + sum_) / (pixel_counter + n_batch_pixels)
+        second_moment = (pixel_counter * second_moment + sum_of_squares) / (pixel_counter + n_batch_pixels)
+        pixel_counter += n_batch_pixels
+
+    mean, std = first_moment, torch.sqrt(second_moment - first_moment ** 2)
+
+    return mean, std
+
+
+def gram_matrix(feature_maps: torch.Tensor) -> torch.Tensor:
+    """
+    Source: https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+    :param feature_maps: F_l, all feature maps of layer l
+    :return: G_l the gram matrix of layer l
+    """
+    batch_size, n_filters, width, height = feature_maps.size()
+
+    # resize F_XL into \hat F_XL
+    features = feature_maps.view(batch_size * n_filters, width * height)
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
+    return G.div(batch_size * n_filters * width * height)
