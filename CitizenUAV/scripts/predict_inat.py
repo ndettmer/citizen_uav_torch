9729933a9c8a0331bbfc164ccab518bbc6cf1373
Niklas Dettmer <ndettmer94@posteo.de>
2023-02-09 16:22:34 +0100

    script for inat predictions

---
+++ CitizenUAV/scripts/predict_inat.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+from CitizenUAV.processes import predict_inat
+from plyer import notification
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--data_dir", type=str, required=True)
+    parser.add_argument("--result_dir", type=str, required=True)
+    parser.add_argument("--img_size", type=int, required=False, default=128)
+    parser.add_argument("--min_distance", type=float, required=False, default=None)
+    parser.add_argument("--gpu", type=bool, required=False, default=None)
+    parser.add_argument("--batch_size", type=int, required=False, default=1)
+    parser.add_argument("--normalize", type=bool, required=False, default=False)
+
+    args = parser.parse_args()
+    dict_args = vars(args)
+    predict_inat(**dict_args)
+
+    notification.notify(
+        title="Inat Predictions",
+        message="Predictions done."
+    )
